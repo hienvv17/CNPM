68be2a41eaba66d6a5797842a06b05db779f16c1
Minh Tùng <minhtung14052001@gmail.com>
2021-11-08 01:38:04 +0700

    1

---
+++ index.php
@@ -0,0 +1,108 @@
+    <div class="container-fluid">
+      
+      <div class="row">
+        <div class="col-md-8 product_info">
+          <h2 class ="text-center menu-title">Món hot</h2>
+
+          <div class="row row-cols-md-4">
+            
+            <div class="col-mb3">
+              <div class="card text-center " >
+                <span class="dishname"> Cơm tấm</span>
+                <img src="img/com_tam.jpg" class="product_img" alt="thumnail" >
+                <div class="card-body">
+                  <h5 class="card-price center ">30000đ</h5>
+                  <input class="btn center" type="button" value="Add">
+                </div>
+              </div>
+            </div>
+            <div class="col">
+              <div class="card text-center">
+                <span class="dishname"> Cơm gà xối xả</span>
+                <img src="img/com_ga.jpg" class="product_img" alt="thumnail">
+                <div class="card-body">
+                  <h5 class="card-price center ">28000đ</h5>
+                  <input class="btn center" type="button" value="Add">
+                </div>
+              </div>
+            </div>
+            <div class="col-mb-3">
+              <div class="card text-center">
+                <span class="dishname">Bún bò Huế</span>
+                <img src="img/bun_bo_hue.jpg" class="product_img" alt="JBL">
+                <div class="card-body">
+                  <h5 class="card-price center ">30000đ</h5>
+                  <input class="btn center" type="button" value="Add">
+                </div>
+              </div>
+            </div>
+            <div class="col-mb-3">
+              <div class="card text-center">
+                <span class="dishname">Mì xào hải sản</span>
+                <img src="img/mi_xao.jpg" class="product_img" alt="JBL">
+                <div class="card-body">
+                  <h5 class="card-price center ">45000đ</h5>
+                  <input class="btn center" type="button" value="Add">
+                </div>
+              </div>
+            </div>
+            <div class="col-mb-3">
+              <div class="card text-center">
+                <span class="dishname">Bún mọc</span>
+                <img src="img/bun_moc.jpg" class="product_img" alt="JBL">
+                <div class="card-body">
+                  <h5 class="card-price center ">28000đ</h5>
+                  <input class="btn center" type="button" value="Add">
+                </div>
+              </div>
+            </div>
+            <div class="col-mb-3">
+              <div class="card text-center">
+                <span class="dishname">Bún thịt nướng</span>
+                <img src="img/bun_thit_nuong.jpg" class="product_img" alt="JBL">
+                <div class="card-body">
+                  <h5 class="card-price center ">25000đ</h5>
+                  <input class="btn center" type="button" value="Add">
+                </div>
+              </div>
+            </div>
+            <div class="col-mb-3">
+              <div class="card text-center">
+                <span class="dishname"> Cocacola </span>
+                <img src="img/coca.jfif" class="product_img" alt="JBL">
+                <div class="card-body">
+                  <h5 class="card-price center ">10000</h5>
+                  <input class="btn center" type="button" value="Add">
+                </div>
+              </div>
+            </div>
+            <div class="col-mb-3">
+              <div class="card text-center">
+                <span class="dishname">Dừa tắc</span>
+                <img src="img/dua_tac.jpg" class="product_img" alt="JBL">
+                <div class="card-body">
+                  <h5 class="card-price center ">12000</h5>
+                  <input class="btn center" type="button" value="Add">
+                </div>
+              </div>
+            </div>
+
+          </div>
+
+        </div>
+
+        <div class="col-sm-3 right">
+
+              <div class="card" style ="height : 100%;">
+                <div class="text-center table-num">Thực đơn bàn</div>
+                <div class="card-body thuc-don">
+                
+                </div>
+                <div class="bottom text-center">
+                  <input type="text" placeholder="Ghi chú..."> <br>
+                <input type="button" class="btn center" value="Hoàn tất">
+                </div>
+              </div>
+          
+        </div>
+    </div>
