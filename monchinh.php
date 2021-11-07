11208d6bf7fea962ff262ad01f4f1e701c3b341c
Minh Tùng <minhtung14052001@gmail.com>
2021-11-07 09:49:08 +0700

    two

---
+++ monchinh.php
@@ -0,0 +1,150 @@
+<div class="container">
+  <div class="row">
+    <div class="col-md-8 product_info">
+      <h2 class="menu-tittle">Món chính</h2>
+      <div class="row row-cols-md-4">
+        <div class="col-mb-3">
+          <div class="card">
+            <span class="dishname"> Cơm tấm</span>
+            <img src="" class="product_img" alt="JBL">
+            <div class="card-body">
+              <h5 class="card-price center ">30000đ</h5>
+              <input class="btn center" type="button" value="Add">
+            </div>
+          </div>
+        </div>
+        <div class="col-mb-3">
+          <div class="card">
+            <span class="dishname"> Cơm gà xối xả</span>
+            <img src="" class="product_img" alt="JBL">
+            <div class="card-body">
+              <h5 class="card-price center ">28000đ</h5>
+              <input class="btn center" type="button" value="Add">
+            </div>
+          </div>
+        </div>
+        <div class="col-mb-3">
+          <div class="card">
+            <span class="dishname">Cơm chiên dương châu</span>
+            <img src="" class="product_img" alt="JBL">
+            <div class="card-body">
+              <h5 class="card-price center ">30000đ</h5>
+              <input class="btn center" type="button" value="Add">
+            </div>
+          </div>
+        </div>
+        <div class="col-mb-3">
+          <div class="card">
+            <span class="dishname">Mì xào hải sản</span>
+            <img src="" class="product_img" alt="JBL">
+            <div class="card-body">
+              <h5 class="card-price center ">45000đ</h5>
+              <input class="btn center" type="button" value="Add">
+            </div>
+          </div>
+        </div>
+        <div class="col-mb-3">
+          <div class="card">
+            <span class="dishname">Mì xào giòn</span>
+            <img src="" class="product_img" alt="JBL">
+            <div class="card-body">
+              <h5 class="card-price center ">28000đ</h5>
+              <input class="btn center" type="button" value="Add">
+            </div>
+          </div>
+        </div>
+        <div class="col-mb-3">
+          <div class="card">
+            <span class="dishname">Bún thịt nướng</span>
+            <img src="" class="product_img" alt="JBL">
+            <div class="card-body">
+              <h5 class="card-price center ">25000đ</h5>
+              <input class="btn center" type="button" value="Add">
+            </div>
+          </div>
+        </div>
+        <div class="col-mb-3">
+          <div class="card">
+            <span class="dishname"> Bún bò Huế </span>
+            <img src="" class="product_img" alt="JBL">
+            <div class="card-body">
+              <h5 class="card-price center ">10000</h5>
+              <input class="btn center" type="button" value="Add">
+            </div>
+          </div>
+        </div>
+        <div class="col-mb-3">
+          <div class="card">
+            <span class="dishname">Bún chả cá</span>
+            <img src="" class="product_img" alt="JBL">
+            <div class="card-body">
+              <h5 class="card-price center ">12000</h5>
+              <input class="btn center" type="button" value="Add">
+            </div>
+          </div>
+        </div>
+        <div class="col-mb-3">
+          <div class="card">
+            <span class="dishname">Bún thịt nướng</span>
+            <img src="" class="product_img" alt="JBL">
+            <div class="card-body">
+              <h5 class="card-price center ">12000</h5>
+              <input class="btn center" type="button" value="Add">
+            </div>
+          </div>
+        </div>
+        <div class="col-mb-3">
+          <div class="card">
+            <span class="dishname">Bún thịt nướng</span>
+            <img src="" class="product_img" alt="JBL">
+            <div class="card-body">
+              <h5 class="card-price center ">12000</h5>
+              <input class="btn center" type="button" value="Add">
+            </div>
+          </div>
+        </div>
+        <div class="col-mb-3">
+          <div class="card">
+            <span class="dishname">Bún thịt nướng</span>
+            <img src="" class="product_img" alt="JBL">
+            <div class="card-body">
+              <h5 class="card-price center ">12000</h5>
+              <input class="btn center" type="button" value="Add">
+            </div>
+          </div>
+        </div>
+        <div class="col-mb-3">
+          <div class="card">
+            <span class="dishname">Bún thịt nướng</span>
+            <img src="" class="product_img" alt="JBL">
+            <div class="card-body">
+              <h5 class="card-price center ">12000</h5>
+              <input class="btn center" type="button" value="Add">
+            </div>
+          </div>
+        </div>
+
+
+      </div>
+
+    </div>
+
+    <div class="col-sm-3 right">
+
+      <div class="card" style="height : 100%;">
+        <div class="text-center table-num">Thực đơn bàn</div>
+        <div class="card-body thuc-don">
+
+        </div>
+        <div class="bottom text-center">
+          <input type="text" placeholder="Ghi chú..."> <br>
+          <input type="button" class="btn center" value="Hoàn tất">
+        </div>
+      </div>
+
+    </div>
+
+
+  </div>
+
+</div>
