328fdb8d3a947ade2ea38ffec2862b0c76db0dc1
bngiahuy <45910202+bngiahuy@users.noreply.github.com>
2021-11-08 14:35:03 +0700

    edit css
    edit little css

--- monthem.php
+++ monthem.php
@@ -1,85 +1,101 @@
 <div class="container">
   <div class="row">
     <div class="col-md-8 product_info">
-      <h2 class="menu-tittle">Món thêm</h2>
-      <div class="row row-cols-md-4">
+      <h2 class="text-center  menu-tittle">Món thêm</h2>
+      <div class="row row-cols-md-4 text-center">
         <div class="col-mb-3">
           <div class="card">
             <span class="dishname">Canh khổ qua</span>
-            <img src="" class="product_img" alt="JBL">
+            <img src="img/kho_qua.jpg" class="product_img" alt="JBL">
             <div class="card-body">
-              <h5 class="card-price center ">15000đ</h5>
-              <input class="btn center" type="button" value="Add">
+              <h5 class="card-price text-center ">15000đ</h5>
+              <a href="#" class="adtocart " id="them_mon" id_mon="1">
+                    <img src="img/cart.jpg" alt="..">
+                </a>
             </div>
           </div>
         </div>
         <div class="col-mb-3">
           <div class="card">
             <span class="dishname">Canh chua cá</span>
-            <img src="" class="product_img" alt="JBL">
+            <img src="img/canh_chua.jpg" class="product_img" alt="JBL">
             <div class="card-body">
-              <h5 class="card-price center ">20000đ</h5>
-              <input class="btn center" type="button" value="Add">
+              <h5 class="card-price text-center ">20000đ</h5>
+              <a href="#" class="adtocart " id="them_mon" id_mon="1">
+                    <img src="img/cart.jpg" alt="..">
+                </a>
             </div>
           </div>
         </div>
         <div class="col-mb-3">
           <div class="card">
             <span class="dishname">Cơm rong biển</span>
-            <img src="" class="product_img" alt="JBL">
+            <img src="img/rong_bien.jpg" class="product_img" alt="JBL">
             <div class="card-body">
-              <h5 class="card-price center ">30000đ</h5>
-              <input class="btn center" type="button" value="Add">
+              <h5 class="card-price text-center ">30000đ</h5>
+              <a href="#" class="adtocart " id="them_mon" id_mon="1">
+                    <img src="img/cart.jpg" alt="..">
+                </a>
             </div>
           </div>
         </div>
         <div class="col-mb-3">
           <div class="card">
             <span class="dishname">Dưa chua</span>
-            <img src="" class="product_img" alt="JBL">
+            <img src="img/dua_chua.png" class="product_img" alt="JBL">
             <div class="card-body">
-              <h5 class="card-price center ">1000đ</h5>
-              <input class="btn center" type="button" value="Add">
+              <h5 class="card-price text-center ">12000đ</h5>
+              <a href="#" class="adtocart " id="them_mon" id_mon="1">
+                    <img src="img/cart.jpg" alt="..">
+                </a>
             </div>
           </div>
         </div>
         <div class="col-mb-3">
           <div class="card">
             <span class="dishname">Kim chi</span>
-            <img src="" class="product_img" alt="JBL">
+            <img src="img/kim_chi.jpg" class="product_img" alt="JBL">
             <div class="card-body">
-              <h5 class="card-price center ">1000đ</h5>
-              <input class="btn center" type="button" value="Add">
+              <h5 class="card-price text-center ">10000đ</h5>
+              <a href="#" class="adtocart " id="them_mon" id_mon="1">
+                    <img src="img/cart.jpg" alt="..">
+                </a>
             </div>
           </div>
         </div>
         <div class="col-mb-3">
           <div class="card">
             <span class="dishname">Rau sống</span>
-            <img src="" class="product_img" alt="JBL">
+            <img src="img/rau_song.jpg" class="product_img" alt="JBL">
             <div class="card-body">
-              <h5 class="card-price center ">5000đ</h5>
-              <input class="btn center" type="button" value="Add">
+              <h5 class="card-price text-center ">5000đ</h5>
+              <a href="#" class="adtocart " id="them_mon" id_mon="1">
+                    <img src="img/cart.jpg" alt="..">
+                </a>
             </div>
           </div>
         </div>
         <div class="col-mb-3">
           <div class="card">
             <span class="dishname">Rau xào</span>
-            <img src="" class="product_img" alt="JBL">
+            <img src="img/rau_xao.jpg" class="product_img" alt="..">
             <div class="card-body">
-              <h5 class="card-price center ">10000</h5>
-              <input class="btn center" type="button" value="Add">
+              <h5 class="card-price text-center ">10000</h5>
+              <a href="#" class="adtocart " id="them_mon" id_mon="1">
+                    <img src="img/cart.jpg" alt="..">
+                </a>
             </div>
           </div>
         </div>
         <div class="col-mb-3">
           <div class="card">
             <span class="dishname">Dưa chua</span>
-            <img src="" class="product_img" alt="JBL">
+            <img src="img/dua_chua.png" class="product_img" alt="..">
             <div class="card-body">
-              <h5 class="card-price center ">12000</h5>
-              <input class="btn center" type="button" value="Add">
+              <h5 class="card-price text-center ">12000</h5>
+              <a href="#" class="adtocart " id="them_mon" id_mon="1">
+                    <img src="img/cart.jpg" alt="..">
+                </a>
             </div>
           </div>
         </div>
@@ -89,16 +105,17 @@
 
     </div>
 
-    <div class="col-sm-3 right">
+    <div class="col-md-4 right">
 
-      <div class="card" style="height : 100%;">
-        <div class="text-center table-num">Thực đơn bàn</div>
-        <div class="card-body thuc-don">
+      <div class="card thuc-don" style="height : 50%;">
+        <div class="text-center table-num">Thực đơn bàn
+        </div>
+        <div class="card-body ">
 
         </div>
         <div class="bottom text-center">
           <input type="text" placeholder="Ghi chú..."> <br>
-          <input type="button" class="btn center" value="Hoàn tất">
+          <button type ="submit" name="hoan_tat">Đặt món</button>
         </div>
       </div>
 
